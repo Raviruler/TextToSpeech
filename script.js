1234860d9f944d436c5cfe0e945da43a5e82a76c
Raviruler <ravikrp22@gmail.com>
2021-04-05 11:29:17 +0530

    JS page

---
+++ script.js
@@ -0,0 +1,14 @@
+function textToAudio() {
+    let msg = document.getElementById("text-to-speech").value;
+    
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 2;
+    speech.voiceURI='native';
+    
+    window.speechSynthesis.speak(speech);
+}
